4c61b5cfd76b6996450a95fb243f03bd0a016abf
anastoffel <162522676+anastoffel@users.noreply.github.com>
2024-03-25 20:28:18 -0300

    Add files via upload

---
+++ PDS.js
@@ -0,0 +1,45 @@
+var numero1 = 5  
+var numero2 = 3
+
+resultado = numero1 == numero2;
+print(numero1, ' = ', numero2, '?', resultado)
+resultado = numero1 != numero2;
+print(numero1, ' != ', numero2, '?', resultado)
+resultado = numero1 > numero2;
+print(numero1, ' > ', numero2, '?', resultado)
+resultado = numero1 < numero2;
+print(numero1, ' < ', numero2, '?', resultado)
+resultado = numero1 >= numero2;
+print(numero1, ' >= ', numero2, '?', resultado)
+resultado = numero1 <= numero2;
+print(numero1, ' <= ', numero2, '?', resultado)
+
+//
+
+var a1 = true && true;
+print(a1);
+var a2 = true && false;
+print(a2);
+var a3 = false && true;
+print(a3);
+var a4 = false && 3 == 4;
+print(a4);
+var a5 = "Gato" && "Cão";
+print(a5);
+var a6 = false && "Gato";
+print(a6);
+var a7 = "Gato" && false;
+print(a7);
+
+//
+
+var idade = 20
+var status = idade >= 18 ? "adulto" : "menor de idade";
+print(status);
+
+var palavra1 = 'alfa'
+var palavra2 = 'beta'
+var resultado = palavra1 + palavra2
+print(resultado)
+
+
